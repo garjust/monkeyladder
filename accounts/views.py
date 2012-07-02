8f635774faa32a2802e79db3b468b45b6ee0d1ec
Justin Garbutt <jagarbut@gmail.com>
2012-07-02 00:42:54 -0400

    activated registration

--- accounts/views.py
+++ accounts/views.py
@@ -14,11 +14,9 @@ def register(request):
 def _do_registration(request):
     form = ExtendedUserCreationForm(request.POST)
     if form.is_valid():
-        print "GOOD FORM"
+        form.save(commit=True)
         form = ExtendedUserCreationForm()
         form.success = True
-    else:
-        print "BAD FORM"
     return render_to_response('accounts/register.html',
         {'form': form},
         context_instance=RequestContext(request)
