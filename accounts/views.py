64af1094400fe13f06f2227e2f5ccb0d0ce837e9
Justin Garbutt <jagarbut@gmail.com>
2012-07-14 22:01:29 -0400

    refactoring accounts templates and home template

--- accounts/views.py
+++ accounts/views.py
@@ -14,15 +14,15 @@ def register(request):
             user = form.save()
             form = ExtendedUserCreationForm()
             form.success = True
-            return render(request, 'accounts/register.html', {'form': form, 'new_user': user})
+            return render(request, 'accounts/register.html', {'register_form': form, 'new_user': user})
     else:
         form = ExtendedUserCreationForm()
-    return render(request, 'accounts/register.html', {'form': form})
+    return render(request, 'accounts/register.html', {'register_form': form})
 
 @login_required(login_url="/accounts/login")
 def view_profile(request, user_id):
     user = logic.get_user_or_404(pk=user_id)
-    return render(request, 'accounts/profile.html', {
+    return render(request, 'accounts/view_profile.html', {
         'user': user, 
         'recent_matches': get_players_match_feed(user, Ladder.objects.get(pk=1)), 
         'ladders': map(lambda p: p.ladder, Player.objects.filter(user=user))
