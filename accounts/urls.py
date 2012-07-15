5d06e2b6a9f50d92d9c1247130b8c516cd9b8557
Justin Garbutt <jagarbut@gmail.com>
2012-07-14 22:28:39 -0400

    cleaned up login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,7 @@ urlpatterns += patterns('django.contrib.auth.views',
 )
 
 urlpatterns += patterns('accounts.views',
-    url(r'^register/$', 'register'),
+    url(r'^register/$', 'register', name='register'),
     url(r'^(?P<user_id>\d+)/profile/$', 'view_profile', name='view_profile'),
     url(r'^(?P<user_id>\d+)/profile/edit/$', 'edit_profile', name='edit_profile'),
 )
