9374153386b3e979c84fc646059d4848cbb99836
jagarbut <garjust@live.ca>
2012-07-22 15:00:25 -0400

    improved profile a bit

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from accounts import logic
 from accounts.forms import ExtendedUserCreationForm
 from core.models import Ladder, Watcher
+from leaderboard.models import Match
 from leaderboard.logic import get_players_match_feed
 
 def register(request):
@@ -20,9 +21,11 @@ def register(request):
 def view_profile(request, user_id):
     user = logic.get_user_or_404(pk=user_id)
     return render(request, 'accounts/view_profile.html', {
-        'user': user, 
-        'recent_matches': get_players_match_feed(user, Ladder.objects.get(pk=1)), 
+        'user': user,
+        'recent_matches': get_players_match_feed(user, Ladder.objects.get(pk=1)),
         'watched_feed': map(lambda l: (l, l.watcher(user)), map(lambda p: p.ladder, Watcher.objects.filter(user=user))),
+        'matches_won': Match.objects.filter(winner=user).count(),
+        'matches_played': Match.objects.filter(winner=user).count() + Match.objects.filter(loser=user).count()
     })
 
 @login_required(login_url="/accounts/login")
