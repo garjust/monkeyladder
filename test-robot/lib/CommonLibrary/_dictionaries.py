e608634626204f4a909758612eb067c3871ffdb4
Justin Garbutt <jagarbut@gmail.com>
2012-07-23 20:27:11 -0400

    added to create ladder tests

--- test-robot/lib/CommonLibrary/_dictionaries.py
+++ test-robot/lib/CommonLibrary/_dictionaries.py
@@ -1,5 +1,7 @@
 from robot_mixin.simple_mixin import SimpleMixin
 
+import random
+
 class _Dictionaries(SimpleMixin):
     """
     Keywords handling dictionary tasks
@@ -7,6 +9,12 @@ class _Dictionaries(SimpleMixin):
     
     def __init__(self):
         super(_Dictionaries, self).__init__()
+
+    def random_string(self, length=6):
+        string = ''
+        for i in range(length):
+            string += str(random.randint(0, 9))
+        return string
 
     def create_dictionary(self, *keyvalues):
         return self._list_to_dictionary(keyvalues)
@@ -18,4 +26,4 @@ class _Dictionaries(SimpleMixin):
 
     def append_random_string_to_dictionary_values(self, dictionary, *keys):
         for key in keys:
-            dictionary[key] = '%s_%s' % (dictionary[key], 1010)
+            dictionary[key] = '%s_%s' % (dictionary[key], self.random_string())
