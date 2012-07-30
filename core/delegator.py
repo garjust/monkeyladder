5f1fe78d30ecc3fbaa5f71f0296c447fd725b253
jagarbut <garjust@live.ca>
2012-07-30 17:36:59 -0400

    refactored delegator dictionary to use dicts over tuples

--- core/delegator.py
+++ core/delegator.py
@@ -14,14 +14,14 @@ EMPTY_CONTEXT_FUNCTION = lambda *args, **kwargs: {}
 
 CONTEXTS = {
     'BASIC': {
-        VIEW: (EMPTY_CONTEXT_FUNCTION, 'core/view_ladder.html'),
-        CONTENT: (EMPTY_CONTEXT_FUNCTION, 'core/content/ladder_display.html'),
-        CONFIG: (LadderConfigurationForm, 'core/configure_ladder.html'),
+        VIEW: {'context': EMPTY_CONTEXT_FUNCTION, 'template': 'core/view_ladder.html'},
+        CONTENT: {'context': EMPTY_CONTEXT_FUNCTION, 'template': 'core/content/ladder_display.html'},
+        CONFIG: {'form': LadderConfigurationForm, 'template': 'core/configure_ladder.html'},
     },
     'LEADERBOARD' : {
-        VIEW: (leaderboard.contexts.view_ladder_context, 'leaderboard/view_ladder.html'),
-        CONTENT: (leaderboard.contexts.ladder_display_context, 'core/content/ladder_display.html'),
-        CONFIG: (LeaderboardConfigurationForm, 'leaderboard/configure_ladder.html'),
+        VIEW: {'context': leaderboard.contexts.view_ladder_context, 'template': 'leaderboard/view_ladder.html'},
+        CONTENT: {'context': leaderboard.contexts.ladder_display_context, 'template': 'core/content/ladder_display.html'},
+        CONFIG: {'form': LeaderboardConfigurationForm, 'template': 'leaderboard/configure_ladder.html'},
     }
 }
 
@@ -34,8 +34,8 @@ def view_ladder(request, ladder_id):
         'ladder': ladder,
         'watcher': logic.get_watcher(request.user, ladder)
     }
-    context.update(CONTEXTS[ladder.type][VIEW][0](request, ladder))
-    return render(request, CONTEXTS[ladder.type][VIEW][1], context)
+    context.update(CONTEXTS[ladder.type][VIEW]['context'](request, ladder))
+    return render(request, CONTEXTS[ladder.type][VIEW]['template'], context)
 
 def ladder_display(request, ladder_id, context={}):
     ladder = logic.get_ladder_or_404(pk=ladder_id)
@@ -45,12 +45,12 @@ def ladder_display(request, ladder_id, context={}):
         'ladder': ladder,
         'watcher': logic.get_watcher(request.user, ladder)
     })
-    context.update(CONTEXTS[ladder.type][CONTENT][0](request, ladder))
-    return render(request, CONTEXTS[ladder.type][CONTENT][1], context)
+    context.update(CONTEXTS[ladder.type][CONTENT]['context'](request, ladder))
+    return render(request, CONTEXTS[ladder.type][CONTENT]['template'], context)
 
 def configure_ladder(request, ladder_id):
     ladder = logic.get_ladder_or_404(pk=ladder_id)
-    form_class =  CONTEXTS[ladder.type][CONFIG][0]
+    form_class =  CONTEXTS[ladder.type][CONFIG]['form']
     if request.POST:
         form = form_class(ladder, request.POST)
         if form.is_valid():
@@ -64,4 +64,4 @@ def configure_ladder(request, ladder_id):
         'watcher': logic.get_watcher(request.user, ladder),
         'form': form,
     }
-    return render(request, CONTEXTS[ladder.type][CONFIG][1], context)
+    return render(request, CONTEXTS[ladder.type][CONFIG]['template'], context)
