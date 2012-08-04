1e94cc15193414f3eb13dd4c62fc9a52f67ab1b7
jagarbut <garjust@live.ca>
2012-08-04 11:22:31 -0400

    tests for core forms

--- core/forms.py
+++ core/forms.py
@@ -34,7 +34,7 @@ class LadderCreationForm(forms.Form):
         )
         Watcher.objects.create(ladder=ladder, user=user, type='ADMIN')
         return ladder
-    
+
 class LadderRankingEditForm(forms.Form):
     """
     A form to edit the rankings of a ladder
@@ -63,7 +63,7 @@ class LadderRankingEditForm(forms.Form):
         for i, ranked in enumerate(self.ladder_ranking):
             ranked.rank = self.cleaned_data['rank_%s' % i]
             ranked.save()
-            
+
 class LadderConfigurationForm(forms.Form):
     """
     A form that edits an existing ladder
@@ -76,15 +76,17 @@ class LadderConfigurationForm(forms.Form):
         self.fields['rungs'].initial = ladder.rungs
         self.fields['is_private'].initial = ladder.is_private
 
-    name = forms.CharField()
-    rungs = forms.IntegerField(min_value=1)
+    name = forms.CharField(required=False)
+    rungs = forms.IntegerField(min_value=1, required=False)
     is_private = forms.BooleanField(required=False)
 
     def clean(self):
         return self.cleaned_data
 
     def save(self):
-        self.ladder.name = self.cleaned_data['name']
-        self.ladder.rungs = self.cleaned_data['rungs']
+        if self.cleaned_data['name']:
+            self.ladder.name = self.cleaned_data['name']
+        if self.cleaned_data['rungs']:
+            self.ladder.rungs = self.cleaned_data['rungs']
         self.ladder.is_private = self.cleaned_data['is_private']
         self.ladder.save()
--- core/tests.py
+++ core/tests.py
@@ -9,7 +9,7 @@ FIXTURES = ['fixtures/users', 'fixtures/core']
 
 class LadderCreationFormTest(TestCase):
     fixtures = FIXTURES
-    
+
     def setUp(self):
         self.fixture = forms.LadderCreationForm
         self.user = User.objects.get(pk=1)
@@ -19,26 +19,47 @@ class LadderCreationFormTest(TestCase):
             'is_private': '0',
             'type': 'LEADERBOARD',
         }
-        
+
     def test_success(self):
         form = self.fixture(self.new_ladder)
         form.is_valid()
         self.assertTrue(form.is_valid())
         self.assertFalse(Ladder.objects.filter(name='New Ladder'), 'Ladder should not exist')
         ladder = form.save(self.user)
-        self.assertEquals(ladder.is_private, False, 'The new ladder is correct')
-        self.assertEquals(ladder.created_by, self.user, 'The new ladder was created by user passed in')
-        self.assertEquals(Watcher.objects.get(ladder=ladder, user=self.user).type, 'ADMIN', 'An admin watcher was created with the ladder and user')
-        
+        self.assertEqual(ladder.is_private, False, 'The new ladder is correct')
+        self.assertEqual(ladder.created_by, self.user, 'The new ladder was created by user passed in')
+        self.assertEqual(Watcher.objects.get(ladder=ladder, user=self.user).type, 'ADMIN', 'An admin watcher was created with the ladder and user')
+
 class LadderRankingEditFormTest(TestCase):
     fixtures = FIXTURES
-    
+
     def setUp(self):
-        self.fixture = LadderRankingEditForm
+        self.fixture = forms.LadderRankingEditForm
         self.ladder = Ladder.objects.get(name='Public Basic')
-        
+
     def test_success(self):
-        form = self.fixture(ladder)
+        form = self.fixture(self.ladder)
+
+class LadderConfigurationFormTest(TestCase):
+    fixtures = FIXTURES
+
+    def setUp(self):
+        self.fixture = forms.LadderConfigurationForm
+        self.ladder = Ladder.objects.get(name='Private Basic')
+
+    def test_success(self):
+        form = self.fixture(self.ladder)
+        self.assertIs(form['name'].value(), self.ladder.name)
+        form_dictionary = {
+            'name': 'Private Basic Configured',
+            'is_private': '0',
+        }
+        form = self.fixture(self.ladder, form_dictionary)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(self.ladder.name, 'Private Basic Configured')
+        self.assertFalse(self.ladder.is_private)
+        self.assertEqual(self.ladder.rungs, 25)
 
 class GetLadderOr404Test(TestCase):
     fixtures = FIXTURES
