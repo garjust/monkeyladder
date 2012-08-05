1a4b4b3b9dae4c89efa48e5ddde00bc6cf5a4b27
Justin Garbutt <jagarbut@gmail.com>
2012-08-04 23:25:09 -0400

    refactored accounts tests

---
+++ accounts/tests/__init__.py
@@ -0,0 +1,4 @@
+from decorators import *
+from forms import *
+from logic import *
+from models import *
---
+++ accounts/tests/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+FIXTURES = ['fixtures/users', 'fixtures/core']
+
+class UserProfileTest(TestCase):
+    fixtures = FIXTURES
+
+    def test_create_user_creates_profile(self):
+        user = User.objects.create(username='bobthecat')
+        self.assertEquals(user.get_profile().name(), 'bobthecat')
+
+    def test_profile_name(self):
+        user = User.objects.create(username='bobthecat', first_name='Bob', last_name='Carr')
+        self.assertEquals(user.get_profile().name(), 'Bob Carr')
