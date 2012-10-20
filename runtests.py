4f1b7bf61b8964c66ff8c8370abfb324acc63870
jagarbut <garjust@live.ca>
2012-10-20 16:36:28 -0400

    pep8

--- runtests.py
+++ runtests.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-import os
 import sys
 
 from django.core.management import execute_from_command_line
+
 
 def run():
     execute_from_command_line([sys.argv[0], 'test', '--verbosity=2', 'accounts'])
