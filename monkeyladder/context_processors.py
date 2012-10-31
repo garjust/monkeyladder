866ed3116719553a839da3e0b0d20d7eb18ae4cd
jagarbut <garjust@live.ca>
2012-10-31 13:47:19 -0400

    bit of matchup work and global announcments

--- monkeyladder/context_processors.py
+++ monkeyladder/context_processors.py
@@ -3,3 +3,9 @@ from monkeyladder.version import VERSION_NUMBER
 
 def version_number(request):
     return {'version_number': VERSION_NUMBER}
+
+
+def global_announcments(request):
+    return {'global_announcments': [
+        ('New version for Ping Pong tournament', ''),
+    ]}
--- monkeyladder/settings/common.py
+++ monkeyladder/settings/common.py
@@ -51,6 +51,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
     'django.contrib.auth.context_processors.auth',
     'monkeyladder.context_processors.version_number',
+    'monkeyladder.context_processors.global_announcments',
 )
 
 STATICFILES_FINDERS = (
