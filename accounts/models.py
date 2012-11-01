0807fc0c963dd72bc6a8f2bfb179759384083f7b
jagarbut <garjust@live.ca>
2012-10-31 20:40:36 -0400

    changed alert close icon

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     about = models.CharField(max_length=500, null=True, blank=True)
@@ -9,6 +10,8 @@ class UserProfile(models.Model):
     def name(self):
         full_name = self.user.get_full_name()
         if full_name:
+            if len(full_name) > 40:
+                return "%s..." % full_name[:40]
             return full_name
         return self.user.username
 
@@ -18,6 +21,7 @@ class UserProfile(models.Model):
 
     def __unicode__(self):
         return "profile for %s" % self.username
+
 
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
