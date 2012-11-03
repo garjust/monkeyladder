a7c173f745607ce5bf24e3b0d5b33f6d243516ab
Justin Garbutt <jagarbut@gmail.com>
2012-11-03 23:29:23 +0000

    upped version

--- monkeyladder/settings/production.py
+++ monkeyladder/settings/production.py
@@ -30,8 +30,8 @@ TIME_ZONE = 'America/Toronto'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jagarbut_ml_1_1_0',
-        'USER': 'jagarbut_ml_1_1_0',
+        'NAME': 'jagarbut_ml_1_1_3',
+        'USER': 'jagarbut_ml_1_1_3',
         'PASSWORD': 'monkeyladderadmin',
         'HOST': '',
         'PORT': '',
