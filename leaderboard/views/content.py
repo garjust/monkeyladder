c6aa01cc7352cdf5daeb79244643e17b7e46e429
jagarbut <garjust@live.ca>
2012-11-03 14:20:24 -0400

    split leaderboard views file

--- leaderboard/views/content.py
+++ leaderboard/views/content.py
@@ -1,28 +1,17 @@
-from django.http import Http404
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator
-from django.views.generic.list_detail import object_detail
-
 from accounts.decorators import login_required_forbidden
 from core.decorators import can_view_ladder, login_required_and_ladder_admin, ladder_is_active
-from core.logic.util import get_ladder_or_404, get_user_or_404, get_page_or_first_page
 from core.generic_views import handle_form_and_redirect_to_ladder, view_with_ladder
-
-from leaderboard.forms import get_match_form, LeaderboardConfigurationForm, LadderRankingAndPlayerEditForm
-from leaderboard.generic_views import view_with_leaderboard
+from core.logic.util import get_ladder_or_404, get_user_or_404, get_page_or_first_page
+from django.core.paginator import Paginator
+from django.http import Http404
+from django.shortcuts import render, redirect
+from django.views.generic.list_detail import object_detail
 from leaderboard import logic
-from leaderboard.models import Match
+from leaderboard.forms import get_match_form, LadderRankingAndPlayerEditForm
+from leaderboard.generic_views import view_with_leaderboard
 from leaderboard.logic.feeds import get_match_feed, climbing_ladder_feed, users_played
 from leaderboard.logic.stats import get_stats
-
-
-@can_view_ladder
-def matches_page(request, ladder_id):
-    ladder = get_ladder_or_404(pk=ladder_id)
-    match_id = request.GET.get('id', None)
-    return view_with_ladder(request, ladder, 'leaderboard/matches_page.html', {
-        'navbar_active': 'matches', 'match_id': match_id, 'match_feed_size': 10,
-    })
+from leaderboard.models import Match
 
 
 @ladder_is_active
@@ -44,13 +33,6 @@ def create_match(request, ladder_id):
 
 @ladder_is_active
 @can_view_ladder
-def ladder_page(request, ladder_id):
-    ladder = get_ladder_or_404(pk=ladder_id)
-    return view_with_leaderboard(request, ladder, 'leaderboard/view_ladder.html', {'navbar_active': 'ladder'})
-
-
-@ladder_is_active
-@can_view_ladder
 def ladder_display(request, ladder_id):
     ladder = get_ladder_or_404(pk=ladder_id)
     return view_with_ladder(request, ladder, 'leaderboard/content/ladder_display.html')
@@ -61,14 +43,6 @@ def ladder_display(request, ladder_id):
 def edit_ladder(request, ladder_id):
     return handle_form_and_redirect_to_ladder(request, ladder_id, LadderRankingAndPlayerEditForm, 'leaderboard/content/ladder_display.html',
         form_name='ladder_edit_form'
-    )
-
-
-@ladder_is_active
-@login_required_and_ladder_admin
-def configure_ladder(request, ladder_id):
-    return handle_form_and_redirect_to_ladder(request, ladder_id, LeaderboardConfigurationForm, 'leaderboard/configure_ladder.html',
-        context={'navbar_active': 'config'}
     )
 
 
