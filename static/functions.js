e97906935e154c5af110a715fcbd8ea629cd1247
jagarbut <garjust@live.ca>
2012-11-03 21:02:39 -0400

    organized javascript a bit

--- static/functions.js
+++ static/functions.js
@@ -17,15 +17,3 @@ function errorTooltip(id, content) {
         placement: "right", title: content
     });
 }
-
-/*
-$(function() {
-	$("a.ajax-load").map(function() {
-		var target = $(this).attr("href")
-		$(this).attr("href", "#")
-		$(this).click(function() {
-			$("#match-entry-span").load($(this).attr("data-load-destination"))
-		})
-	})
-})
-*/
---
+++ static/onload.js
@@ -0,0 +1,11 @@
+/*
+$(function() {
+	$("a.ajax-load").map(function() {
+		var target = $(this).attr("href")
+		$(this).attr("href", "#")
+		$(this).click(function() {
+			$("#match-entry-span").load($(this).attr("data-load-destination"))
+		})
+	})
+})
+*/
