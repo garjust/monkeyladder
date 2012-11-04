fc7abc169b088c59519fa40025c0c183c54ac5e4
jagarbut <garjust@live.ca>
2012-11-03 20:48:30 -0400

    extracted common js function

--- static/js.js
+++ static/js.js
@@ -17,3 +17,15 @@ function errorTooltip(id, content) {
         placement: "right", title: content
     });
 }
+
+/*
+$(function() {
+	$("a.ajax-load").map(function() {
+		var target = $(this).attr("href")
+		$(this).attr("href", "#")
+		$(this).click(function() {
+			$("#match-entry-span").load($(this).attr("data-load-destination"))
+		})
+	})
+})
+*/
