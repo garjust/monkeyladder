3d5a4d0ba28b9d64168d396adf36678ec6b8cb6b
jagarbut <garjust@live.ca>
2012-11-06 14:13:39 -0500

    more cleanup

--- leaderboard/views/content.py
+++ leaderboard/views/content.py
@@ -4,7 +4,7 @@ from core.generic_views import view_with_ladder, handle_form
 from core.logic.pagination import get_page, get_page_with_item
 from core.logic.util import get_ladder_or_404, get_user_or_404, int_or_none, empty_string_if_none
 from django.core.paginator import Paginator
-from django.http import Http404
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render
 from django.views.generic.list_detail import object_detail
 from leaderboard.decorators import ladder_player_or_admin
@@ -19,7 +19,7 @@ from leaderboard.models import Match
 @ladder_player_or_admin
 def create_match(request, ladder_id):
     ladder = get_ladder_or_404(pk=ladder_id)
-    games = request.GET.get('games')
+    games = int_or_none(request.GET.get('games'))
     if request.POST:
         form = get_match_form(ladder, post_dictionary=request.POST, number_of_games=games)
         if form.is_valid():
@@ -52,9 +52,6 @@ def edit_ladder(request, ladder_id):
 
 @login_required_forbidden
 def matchups(request):
-    """
-    Return a piece of HTML designed to be embedded in the matchup container
-    """
     ladder_id = empty_string_if_none(int_or_none(request.GET.get('ladder_id')))
     user_id = empty_string_if_none(int_or_none(request.GET.get('user_id')))
     page_number = empty_string_if_none(int_or_none(request.GET.get('page')))
@@ -72,17 +69,14 @@ def matchups(request):
     }
 
     paginator = Paginator(users_played(user_id=user_id, ladder_id=ladder_id), size)
-    matchup_users = get_page(paginator, page_number)
+    page = get_page(paginator, page_number)
+    context['feed'] = page
 
-    matchups = []
-    for matchup_user in matchup_users:
-        matchup = get_stats(user_id, ladder=ladder_id, other_user_id=matchup_user.id)
-        matchup['user'] = matchup_user
+    for matchup_user in page:
+        matchup_user.stats = get_stats(user_id, ladder=ladder_id, other_user_id=matchup_user.id)
+        matchup_user.user = matchup_user
         if ladder_id:
-            matchup['ladder_id'] = ladder_id
-        matchups.append(matchup)
-    context['feed'] = matchup_users
-    context['feed_data'] = matchups
+            matchup_user.ladder = ladder
 
     context['feed_info'] = {
         'name': 'Matchups',
@@ -115,12 +109,9 @@ def matches(request):
         user = get_user_or_404(pk=user_id)
 
     paginator = Paginator(get_match_feed(ladder=ladder_id, user=user_id), size)
-    if match_id:
-        matches = get_page_with_item(paginator, match_id)
-    else:
-        matches = get_page(paginator, page_number)
+    page = get_page_with_item(paginator, match_id) if match_id else get_page(paginator, page_number)
 
-    context = {'feed': matches, 'match_feed_size': size,
+    context = {'feed': page, 'match_feed_size': size,
         'match_id': match_id,
         'feed_ladder': ladder if ladder_id else ladder_id,
         'matches_user': user if user_id else user_id,
@@ -142,22 +133,25 @@ def matches(request):
     return render(request, 'leaderboard/content/match_feed.html', context)
 
 
+def match(request, match_id):
+    return object_detail(request, Match.objects.filter(pk=match_id), match_id,
+        template_name='leaderboard/content/match.html',
+        template_object_name='match'
+    )
+
+
 @login_required_forbidden
 def stats(request):
     """
     Returns statistics for the given user
     """
-    user_id = request.GET.get('user_id')
-    ladder_id = request.GET.get('ladder_id')
+    user_id = empty_string_if_none(int_or_none(request.GET.get('user_id')))
+    ladder_id = empty_string_if_none(int_or_none(request.GET.get('ladder_id')))
     if not user_id:
-        raise Http404()
+        return HttpResponseBadRequest()
     context = {'stats_user': get_user_or_404(pk=user_id), 'stats_ladder': None}
     if ladder_id:
         context['stats_ladder'] = get_ladder_or_404(pk=ladder_id)
     context['stats_user_ladders'] = get_played_ladder_feed(context['stats_user'], order='name')
     context['stats'] = get_stats(user_id, ladder=ladder_id)
     return render(request, 'leaderboard/content/player_stats.html', context)
-
-
-def match(request, match_id):
-    return object_detail(request, Match.objects.filter(pk=match_id), match_id, template_name='leaderboard/content/match.html', template_object_name='match')
